52d6427075eace93cf9bfe8db2459b65cad1fd0f
Guillaume Gaullier <guillaume@gaullier.org>
2019-11-12 13:36:57 -0700

    Added UI customizations

---
+++ config.el
@@ -0,0 +1,7 @@
+;;; ~/.doom.d/config.el -*- lexical-binding: t; -*-
+
+;; Place your personal configuration here
+
+(add-to-list 'load-path "~/.doom.d/config")
+
+(load "ui")
---
+++ config/ui.el
@@ -0,0 +1,25 @@
+;;; ~/.doom.d/config/ui.el -*- lexical-binding: t; -*-
+
+;; Place your UI customizations here
+
+;; Modifier keys on macOS.
+(when (eq system-type 'darwin)
+  (setq mac-command-modifier 'meta
+        mac-option-modifier  'control
+        mac-control-modifier 'super
+        ns-function-modifier 'hyper))
+
+;; Theme.
+(use-package! doom-themes
+  :config
+  (load-theme 'doom-solarized-light t)
+  (setq doom-themes-enable-bold t
+        doom-themes-enable-italic t)
+  (doom-themes-visual-bell-config)
+  (doom-themes-org-config))
+
+;; Other interface customizations.
+(add-to-list 'default-frame-alist '(height . 54))
+(add-to-list 'default-frame-alist '(width . 100))
+(setq scroll-preserve-screen-position t
+      confirm-nonexistent-file-or-buffer nil)

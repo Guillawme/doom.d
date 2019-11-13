f32a249a04c8123c7ba49263a96398ac936a41aa
Guillaume Gaullier <guillaume@gaullier.org>
2019-11-13 12:19:38 -0700

    Added configuration for interactive citation from a main bib file

--- config/keybindings.el
+++ config/keybindings.el
@@ -2,6 +2,9 @@
 
 ;; Place your custom keybindings here
 
+;; Search bibliography entry.
+(bind-key "C-c c" 'ivy-bibtex)
+
 ;; Move by paragraph.
 (bind-keys*
  ("M-[" . backward-paragraph)
--- config/latex.el
+++ config/latex.el
@@ -2,5 +2,17 @@
 
 ;; Settings for latex mode
 
-;; Bibliography file.
-(setq reftex-default-bibliography "~/Labo/zotero-library.bib")
+;; Bibliography and citation settings.
+(setq reftex-default-bibliography "~/Labo/zotero-library.bib"
+      bibtex-completion-bibliography '("~/Labo/zotero-library.bib")
+      bibtex-completion-pdf-field "file"
+      bibtex-completion-notes-path "~/Labo/zotero-notes"
+      bibtex-completion-notes-extension ".md"
+      bibtex-completion-find-additional-pdfs t
+      bibtex-completion-format-citation-functions
+      '((org-mode      . bibtex-completion-format-citation-org-link-to-PDF)
+        (latex-mode    . bibtex-completion-format-citation-cite)
+        (markdown-mode . bibtex-completion-format-citation-pandoc-citeproc)
+        (default       . bibtex-completion-format-citation-default))
+      ivy-bibtex-default-action 'ivy-bibtex-insert-citation)
+

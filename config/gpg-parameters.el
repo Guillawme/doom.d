1aebec50df2954ed2cc2537bdd18046d9f82459b
removedporn <86824510+removedporn@users.noreply.github.com>
2022-06-07 18:29:06 +0800

    Delete config directory

--- config/gpg-parameters.el
+++
@@ -1,7 +0,0 @@
-;;; ~/.doom.d/config/gpg-parameters.el -*- lexical-binding: t; -*-
-
-;; Place personal GnuPG parameters here
-
-(setq-default epg-gpg-program "gpg"
-              epg-gpg-home-directory "~/.gnupg/"
-              epa-pinentry-mode 'loopback)
--- config/org-mode.el
+++
@@ -1,5 +0,0 @@
-;;; ~/.doom.d/config/org-mode.el -*- lexical-binding: t; -*-
-
-;; Place your org-mode settings here
-
-(setq org-directory "~/org")

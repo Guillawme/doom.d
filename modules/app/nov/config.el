7169c6cf14d24659bac923f571802fd133a7a4fd
removedporn <86824510+removedporn@users.noreply.github.com>
2022-06-07 18:29:45 +0800

    Delete modules directory

--- modules/app/nov/config.el
+++
@@ -1,8 +0,0 @@
-;;; app/nov/config.el -*- lexical-binding: t; -*-
-
-;; Configuration of the nov.el epub reader
-
-(use-package! nov
-  :mode ("\\.epub\\'" . nov-mode)
-  :config
-  (setq nov-save-place-file (concat doom-cache-dir "nov-places")))

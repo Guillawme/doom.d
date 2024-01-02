db5f82521d145b8c251d0a45836a35f0f562260a
Guillaume Gaullier <guillaume@gaullier.org>
2024-01-02 18:11:06 +0100

    Update global parameters

--- config/global-parameters.el
+++ config/global-parameters.el
@@ -6,3 +6,11 @@
 ;; kill-ring with duplicates.
 (setq-default kill-read-only-ok t
               kill-do-not-save-duplicates t)
+
+;; Delete files by moving to the OS trash instead of removing right away
+(setq trash-directory "~/.Trash")
+;; See `trash-directory' as it requires defining `system-move-file-to-trash'.
+(defun system-move-file-to-trash (file)
+  "Use \"trash\" to move FILE to the system trash."
+  (cl-assert (executable-find "trash") nil "'trash' must be installed. Needs \"brew install trash\"")
+  (call-process "trash" nil 0 nil "-F"  file))

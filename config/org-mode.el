a426403dd121900fefe6b2628ccdb51c062dfa34
Guillaume Gaullier <guillaume@gaullier.org>
2024-01-02 18:11:51 +0100

    Update org-mode config

--- config/org-mode.el
+++ config/org-mode.el
@@ -2,4 +2,11 @@
 
 ;; Place your org-mode settings here
 
-(setq org-directory "~/org")
+(setq org-directory "~/org/"
+      org-attach-id-dir "attachments/")
+
+(setq-default org-download-timestamp "%Y-%m-%d_%H-%M-%S_")
+
+;org-download-image-dir (concat org-directory "/img-downloads")
+;
+;
